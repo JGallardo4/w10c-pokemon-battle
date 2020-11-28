ad20c95363bc1b495c73b1d5ebb5f650dbb9bf11
Juan Gallardo <juan.gallardo.ab@gmail.com>
2020-11-28 10:23:09 -0700

    Add list of divas

---
+++ js/app.js
@@ -0,0 +1,61 @@
+/* Queens: 
+
+Starter:
+Dua Lippa
+Camila Cabello
+Rebecca Black
+
+Classic:
+Mariah
+Whitney
+Dolly Parton
+Aretha Franklin
+Donna Summers
+Tina Turner
+Stevie Nicks
+Celine Dion
+Janet Jackson
+Cher
+
+Pop: 
+Gaga
+Ari
+Taylor
+Billie
+Nelly Furtado
+Kylie
+Katy Perry
+Britney
+Madona
+Miley
+Adele
+Cristina
+Gwent stefani
+J-Lo
+Pink
+Marina
+Beyonce
+
+Rap:
+Nicki
+Doja
+Megan D Stallion
+Iggy Azelia
+Lizzo
+FKA Twigs
+Missy Elliot
+Cardi B
+Ash Nikko
+
+Indie:
+Caroline Polachek
+Feist
+Tegan and Sarah
+Grimes
+Bjork
+St Vincent
+Lana del Rey
+Karen O
+Emily Haines
+Imogen Heap
+*/
